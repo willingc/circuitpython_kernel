abe08a8b4cf9f5e86cd36b5ccc0aae1cf3513bb3
brentru <robots199@me.com>
2018-06-08 13:26:08 -0400

    added in and tested moar boards

--- circuitpython_kernel/board.py
+++ circuitpython_kernel/board.py
@@ -12,66 +12,27 @@ FEATHER_MO_WIFI = {'VID': 0x239A, 'PID': 0x801b}
 TRINKET_MO = {'VID': 0x239A, 'PID': 0x801F}
 METRO_M0 = {'VID': 0x239A, 'PID': 0x8014}
 METRO_M4 = {'VID': 0x239A, 'PID': 0x8021}
-CPX_M0 = {'VID': 0x239A, 'PID': 0x8018}
+CPX_M0 = {'VID': 0x239A, 'PID': 0x8019}
 
-# ESP Boards USB (SLAB_USBtoUART)
-HUZZAH_8266 = {'VID': 0x10C4, 'PID': 0xEA60}
+#TODO: Add in ESP Boards
 
 BAUDRATE = 115200
 PARITY = 'N'
 
 
 def find_board():
-    """Find connected board port
-
-    Returns
-    -------
-    port.device
-        Serial object connected to the microcontroller board
-    is_samd
-        bool: samd or non-samd (ESP-based) board type
-    """
-    is_samd = True
+    """Find port where first board is connected."""
     for port in comports():
-        # check if SAMD-based
-        if (
-            (
-                port.vid == FEATHER_MO_BASIC['VID']
-                and port.pid == FEATHER_MO_BASIC['PID']
-            )
-            or (
-                port.vid == FEATHER_MO_EXPRESS['VID']
-                and port.pid == FEATHER_MO_EXPRESS['PID']
-            )
-            or (
-                port.vid == TRINKET_MO['VID']
-                and port.pid == TRINKET_MO['PID']
-            )
-            or (
-                port.vid == METRO_M4['VID']
-                and port.pid == METRO_M4['PID']
-            )
-            or (
-                port.vid == CPX_M0['VID']
-                and port.pid == CPX_M0['PID']
-            )
-            or (
-                port.vid == FEATHER_MO_WIFI['VID']
-                and port.pid == FEATHER_MO_WIFI['PID']
-            )
-        ):
-            #return port.device
-            is_samd = True
-        # check if ESP-based
-        elif (
-                (
-                    port.vid == HUZZAH_8266['VID']
-                    and port.pid == HUZZAH_8266['PID']
-                )
-        ):
-            is_samd = False
-        return is_samd
-        return port.device
+        print('vid: ', port.vid)
+        print('pid: ', port.pid)
+        # checking for SAMD21-boards
+        if ((port.vid == FEATHER_MO_BASIC['VID'] and port.pid == FEATHER_MO_BASIC['PID']) or
+            (port.vid == FEATHER_MO_EXPRESS['VID'] and port.pid == FEATHER_MO_EXPRESS['PID']) or
+            (port.vid == TRINKET_MO['VID'] and port.pid == TRINKET_MO['PID']) or
+            (port.vid == CPX_M0['VID'] and port.pid == CPX_M0['PID']) or
+            (port.vid == FEATHER_MO_WIFI['VID'] and port.pid == FEATHER_MO_WIFI['PID']) or
+            (port.vid == METRO_M0['VID'] and port.pid == METRO_M0['PID'])):
+            return port.device
 
 
 def connect():
@@ -114,5 +75,4 @@ def connect():
     s.write(b'\x03\x01')  # Ctrl-C: interrupt, Ctrl-A: switch to raw REPL
     s.read_until(b'raw REPL')
     s.read_until(b'\r\n>')  # Wait for prompt
-    print (s)
     return s
